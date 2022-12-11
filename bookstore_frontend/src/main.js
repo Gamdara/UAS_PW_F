6cac361d8d72f4d0594a502b0fbcf075f84a2821
Frederikus Mardiyanto W <94357450+frederikusmardiyantow@users.noreply.github.com>
2022-12-11 19:55:56 +0700

    add user HomePage + Footer

--- bookstore_frontend/src/main.js
+++ bookstore_frontend/src/main.js
@@ -2,7 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import vuetify from './plugins/vuetify'
+import vuetify from '@/plugins/vuetify'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'vue-navigation-bar/dist/vue-navigation-bar.css'
+import VueNavigationBar from 'vue-navigation-bar'
+import '@mdi/font/css/materialdesignicons.css'
+import { BootstrapVue, IconsPlugin, NavbarPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.use(NavbarPlugin)
+
+Vue.component('vue-navigation-bar', VueNavigationBar)
 
 Vue.config.productionTip = false
 
